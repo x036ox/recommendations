b79aa765f2cc4e5ca8ec9639a9e10e5be16dce92
Artur <arttturyyy@gmail.com>
2024-08-03 23:53:20 +0200

    Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:21-jdk-slim
+WORKDIR recommendations
+COPY /target/recommendations-1.0.0.jar /recommendations
+CMD ["java", "-jar", "recommendations-1.0.0.jar"]
--- src/test/java/com/artur/recommendations/RecommendationsServiceApplicationTests.java
+++ src/test/java/com/artur/recommendations/RecommendationsServiceApplicationTests.java
@@ -1,9 +1,11 @@
-package com.artur.recommendations_service;
+package com.artur.recommendations;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class RecommendationsServiceApplicationTests {
 
 	@Test
